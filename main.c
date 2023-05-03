3450f0159d4f0a591edc61a2a46842e843385d7b
RNatali <132484336+RNatali@users.noreply.github.com>
2023-05-03 20:01:26 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,52 @@
-#include <stdio.h>
+#include"arr.h"
+#include"task1.h"
+#include"task2.h"
+#include"task3.h"
+#include"task4.h"
+#include"task5.h"
 
-void read_array(int arr[], int size, const char name[]) {
-//зчитує масив та присвоює його до масиву arr 
-    printf("Введіть масив %s:\n", name);
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
+void print_menu();
+int run_task(int *num_task, int *pa, int *pb, int **pa5,  int n, int pn5);
+void choose_task(int *num_task);
+
+
+int main(){
+    int *a, *b, **a5;
+    int n, n5;
+    int num_task;
+
+    print_menu();
+    run_task(&num_task, a, b, a5, n, n5);
+}
+
+void print_menu(){
+    printf("______________________________________________________________________________________________________________________\n");
+    printf("Доступні функції:\n");
+    printf("1.task_1\n");
+    printf("2.task_2\n");
+    printf("3.task_3\n");
+    printf("4.task_4\n");
+    printf("5.task_5\n");
+    printf("Якщо бажаєте вийти з меню натисніть 0\n");
+}
+
+void choose_task(int *num_task){
+    scanf("%d",num_task);
+}
+int run_task(int *num_task, int *pa, int *pb, int **pa5,  int n, int pn5){
+
+    while(1){
+        printf("Виберіть номер функції: ");
+        choose_task(num_task);
+
+
+        switch(*num_task){
+            case 1: task_1(pa, n); break;
+            case 2: task_2(pa, n); break;
+            case 3: task_3(pa, n); break;
+            case 4: task_4(pa, n); break;
+            case 5: task_5(pa5, pn5); break;
+            default: printf("Программа закінчена"); return 0 ; break;
+        }
     }
 }
-
-void print_array(int arr[], int size) {
-//функція яка вводить переданий масив arr
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-void sum_diff_arrays(int a[], int b[], int c[], int n) {
-//функція яка обчислює суму та різницю
-    for (int i = 0; i < n; i++) {
-        c[i] = a[i] + b[i];
-        c[i+n] = a[i+n] - b[i+n];
-    }
-}
-
-void process_arrays() {
-    int n;
-    printf("Введіть значення n: ");
-    scanf("%d", &n);
-    int a[2*n], b[2*n], c[2*n];
-    read_array(a, 2*n, "A");
-    read_array(b, 2*n, "B");
-    sum_diff_arrays(a, b, c, n);
-    printf("Масив С:\n");
-    print_array(c, 2*n);
-}
-
-int main (){
-    process_arrays();
-}
-
-
-
